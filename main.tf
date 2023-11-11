3a7cde0201fdc6ee3d82981b29cdcf370729325f
vlad bronfman <root_admin@PCBANG>
2023-11-11 12:38:06 +0200

    fixed deployment;added ingress block

--- main.tf
+++ main.tf
@@ -1,26 +1,18 @@
 #https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs/guides/getting-started
+# https://aperogeek.fr/kubernetes-deployment-with-terraform/
 
-terraform {
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.0"
-    }
-  }
-}
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-}
-resource "kubernetes_namespace" "test" {
+resource "kubernetes_namespace" "challenge" {
   metadata {
     name = "decoya-assignment"
   }
 }
-resource "kubernetes_deployment" "test" {
+
+resource "kubernetes_deployment" "challenge" {
   metadata {
     name      = "decoya-assignment"
-    namespace = kubernetes_namespace.test.metadata.0.name
+    namespace = kubernetes_namespace.challenge.metadata.0.name
   }
+
   spec {
     replicas = 1
     selector {
@@ -28,6 +20,11 @@ resource "kubernetes_deployment" "test" {
         app = "DecoyaAssignment"
       }
     }
+
+    strategy {
+      type = "Recreate"
+    }
+
     template {
       metadata {
         labels = {
@@ -44,6 +41,17 @@ resource "kubernetes_deployment" "test" {
           volume_mount {
             name       = "share-vol"
             mount_path = "/var/www/html/config"
+          }
+
+          liveness_probe {
+            tcp_socket {
+              port = 8080
+            }
+            failure_threshold     = 3
+            initial_delay_seconds = 3
+            period_seconds        = 10
+            success_threshold     = 1
+            timeout_seconds       = 2
           }
           
         }
@@ -68,14 +76,14 @@ resource "kubernetes_deployment" "test" {
     }
   }
 }
-resource "kubernetes_service" "test" {
+resource "kubernetes_service" "challenge" {
   metadata {
     name      = "decoya-assignment-svc"
-    namespace = kubernetes_namespace.test.metadata.0.name
+    namespace = kubernetes_namespace.challenge.metadata.0.name
   }
   spec {
     selector = {
-      app = kubernetes_deployment.test.spec.0.template.0.metadata.0.labels.app
+      app = kubernetes_deployment.challenge.spec.0.template.0.metadata.0.labels.app
     }
     type = "NodePort"
     port {
@@ -85,3 +93,28 @@ resource "kubernetes_service" "test" {
     }
   }
 }
+
+
+# Ingress
+# resource "kubernetes_ingress" "challenge" {
+#   metadata {
+#     annotations {
+#       "kubernetes.io/ingress.class"                 = "nginx"
+#     }
+#     name      = "decoya-assignment-ing"
+#     namespace = "${kubernetes_namespace.monitoring.metadata.0.name}"
+#   }
+#   spec {
+#     rule {
+##       host = "grafana.aperogeek.fr"
+#       http {
+#         path {
+#           backend {
+#             service_name = "${kubernetes_service.decoya-assignment-svc.metadata.0.name}"
+#             service_port = 80
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
